34c1828b8b574f60bf5ffcfd8ac7bff2e1fd285b
Quico Orozco <esi@plumbaria.com>
2019-01-11 19:00:59 +0100

    Commit inicial Ejercicio 5.11. Sensores disponibles

---
+++ app/src/main/java/com/plumbaria/e_5_11_sensores/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.plumbaria.e_5_11_sensores
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
