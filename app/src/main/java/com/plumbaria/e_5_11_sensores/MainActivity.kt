fa2a25ac8ed10fb9d24179f72daefe9a54962c8e
Quico Orozco <esi@plumbaria.com>
2019-01-15 18:45:17 +0100

    Terminado Ejercicio 5.13. Obteniendo la orientación

--- app/src/main/java/com/plumbaria/e_5_11_sensores/MainActivity.kt
+++ app/src/main/java/com/plumbaria/e_5_11_sensores/MainActivity.kt
@@ -5,31 +5,35 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ScrollView
+import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var salida:TextView
-
+    private var mGravedad:FloatArray? = null
+    private var mGeomagnetismo:FloatArray? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        salida = this.findViewById(R.id.salida)
+        salida = findViewById(R.id.salida)
         var sensorManager:SensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        /*
+
         var listaSensores:List<Sensor> = sensorManager.getSensorList(Sensor.TYPE_ALL)
+        /*
         for (sensor in listaSensores){
             log(sensor.name)
         }
         */
+
+        /*
         var listaSensores:List<Sensor> = sensorManager.getSensorList(Sensor.TYPE_ORIENTATION)
         if (!listaSensores.isEmpty()){
             var orientationSensor:Sensor = listaSensores.get(0)
             sensorManager.registerListener(this, orientationSensor, SensorManager.SENSOR_DELAY_UI)
         }
+
+        */
         listaSensores = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER)
         if(!listaSensores.isEmpty()) {
             var acelerometerSensor = listaSensores.get(0)
@@ -57,17 +61,14 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     override fun onSensorChanged(event: SensorEvent?) {
         synchronized(this){
             when(event?.sensor?.type){
-                Sensor.TYPE_ORIENTATION ->{
-                    for (i in 0..2) {
-                        log("Orientación" + i + ":" + event.values[i])
-                    }
-                }
                 Sensor.TYPE_ACCELEROMETER ->{
+                    mGravedad = event.values
                     for (i in 0..2) {
                         log("Acelerómetro" + i + ":" + event.values[i])
                     }
                 }
                 Sensor.TYPE_MAGNETIC_FIELD->{
+                    mGeomagnetismo = event.values
                     for (i in 0..2) {
                         log("Magnetismo" + i + ":" + event.values[i])
                     }
@@ -76,6 +77,21 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                     for (i in 0..(event?.values?.size!!)) {
                         log("Temperatura" + i + ":" + event.values[i])
                     }
+                }
+                /* OBTENIENDO LA ORIENTACIÓN */
+            }
+
+            if (mGeomagnetismo != null && mGravedad !=null) {
+                var R:FloatArray = FloatArray(9)
+                var I:FloatArray = FloatArray(9)
+                var exito:Boolean = SensorManager.getRotationMatrix(R,I,mGravedad,mGeomagnetismo)
+                if (exito) {
+                    var orientacion:FloatArray = FloatArray(3)
+                    SensorManager.getOrientation(R,orientacion)
+                    // ángulos en radianes
+                    log("ORIENTACION Acimut " + orientacion[0])
+                    log("ORIENTACION Pitch " + orientacion[1])
+                    log("ORIENTACION Roll " + orientacion[2])
                 }
             }
         }
